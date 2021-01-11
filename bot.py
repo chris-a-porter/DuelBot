80714a4405ce9e7e8eafdd516026e34fae493a70
ᴛ̶ᴜ̶ʀ̶ʙ̶ᴏ̶ <68685674+imTURBObro@users.noreply.github.com>
2021-01-11 05:29:45 -0600

    prefix change to =

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-bot = discord.ext.commands.Bot(command_prefix = '\')
+bot = discord.ext.commands.Bot(command_prefix = '=p')
 duels = {}
 lastMessages = {}
 DATABASE_URL = os.environ['DATABASE_URL']
